cf89f71d967ed587b633144b67d327a468aeecf0
vrma <vrmachado@gmail.com>
2026-07-08 19:56:17 +0200

    Antes de crear la rama destinos-diferentes

---
+++ gestion-de-vuelos-reducido/src/main/java/com/example/Vuelo.java
@@ -0,0 +1,34 @@
+package com.example;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+@EqualsAndHashCode
+@ToString
+public class Vuelo {
+	
+	private Destino destino;
+	private BigDecimal precio;
+	private LocalDate fechaSalida;
+	private LocalTime horaSalida;
+	private LocalDate fechaLlegada;
+	private LocalTime horaLlegada;
+	private int numeroPlazas;
+	
+	private List<Pasajero> pasajeros;
+}
