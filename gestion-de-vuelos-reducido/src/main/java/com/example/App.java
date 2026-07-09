2a0db9464c8a991cc9e3572549fc289fe54c37fd
vrma <vrmachado@gmail.com>
2026-07-09 21:00:21 +0200

    Resuelto hasta el Pto 7

--- gestion-de-vuelos-reducido/src/main/java/com/example/App.java
+++ gestion-de-vuelos-reducido/src/main/java/com/example/App.java
@@ -2,11 +2,17 @@ package com.example;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.Month;
+import java.time.temporal.ChronoUnit;
+import java.time.temporal.TemporalAdjusters;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class App {
 	public static void main(String[] args) {
@@ -21,8 +27,8 @@ public class App {
 		// Vuelo 1 con destino PARIS
 
 		Vuelo vuelo1 = Vuelo.builder().destino(Destino.PARIS).precio(new BigDecimal(150.0))
-				.fechaSalida(LocalDate.of(2026, Month.JULY, 8)).horaSalida(LocalTime.of(23, 30))
-				.fechaLlegada(LocalDate.of(2026, Month.JULY, 9)).horaLlegada(LocalTime.of(12, 20)).numeroPlazas(3)
+				.fechaSalida(LocalDate.now()).horaSalida(LocalTime.of(23, 30))
+				.fechaLlegada(LocalDate.now().plusDays(1)).horaLlegada(LocalTime.of(12, 20)).numeroPlazas(3)
 				.build();
 		
 		// Pasajeros del vuelo 1
@@ -63,8 +69,8 @@ public class App {
 		// Vuelo 2 con destino MIAMI
 
 		Vuelo vuelo2 = Vuelo.builder().destino(Destino.MIAMI).precio(new BigDecimal(800.0))
-				.fechaSalida(LocalDate.of(2026, Month.JULY, 9)).horaSalida(LocalTime.of(22, 30))
-				.fechaLlegada(LocalDate.of(2026, Month.JULY, 10)).horaLlegada(LocalTime.of(14, 20)).numeroPlazas(3)
+				.fechaSalida(LocalDate.of(2026, Month.JULY, 25)).horaSalida(LocalTime.of(22, 30))
+				.fechaLlegada(LocalDate.of(2026, Month.JULY, 26)).horaLlegada(LocalTime.of(14, 20)).numeroPlazas(3)
 				.build();
 		
 		// Pasajeros del vuelo 2
@@ -125,6 +131,132 @@ public class App {
 		// Podemos construir la lista de vuelos 
 		
 		vuelos = Arrays.asList(vuelo1, vuelo2, vuelo3);
+		
+		// Solucion al Ejercicio Preliminar
+		
+		Vuelo vueloConMasPasajeros = vuelos.stream()
+				.max((v1,v2) -> Integer.valueOf(v1.getPasajeros().size())
+						 .compareTo(v2.getPasajeros().size()))
+				.orElse(null);
+		
+		System.out.println("Vuelo con mas pasajeros es: ");
+		System.out.println(vueloConMasPasajeros);
+		
+		// Pto 1.
+		
+		List<Vuelo> vuelosCompleto = vuelos.stream()
+				.filter(vuelo -> vuelo.getPasajeros().size() == vuelo.getNumeroPlazas())
+				.collect(Collectors.toList());
+		
+		// Modernamente, año 2026. No hace falta el metodo collect al final de la tuberia
+		// si quieres obtener una lista inmutable
+		
+		List<Vuelo> vuelosCompleto2 = vuelos.stream()
+				.filter(vuelo -> vuelo.getPasajeros().size() == vuelo.getNumeroPlazas())
+				.toList();
+		
+		System.out.println("Listado de vuelos completos ");
+		System.out.println(vuelosCompleto2);
+		
+		// Pto 2
+		
+		List<Vuelo> vuelosHoy = vuelos.stream()
+				.filter(vuelo -> vuelo.getFechaSalida().isEqual(LocalDate.now()))
+				.toList();
+		
+		System.out.println("Vuelos con salida para el dia de hoy: ");
+		System.out.println(vuelosHoy);
+		
+		// Pto 3
+		
+		List<Vuelo> vuelosMasDe10Horas = vuelos.stream()
+				.filter(vuelo -> ChronoUnit.HOURS
+				    .between(LocalDateTime.of(vuelo.getFechaSalida(), vuelo.getHoraSalida()), 
+						  LocalDateTime.of(vuelo.getFechaLlegada(), vuelo.getHoraLlegada())) > 10)
+				.toList();
+				
+		System.out.println("Vuelos que demoran mas de 10 horas en llegar a su destino ");
+		System.out.println(vuelosMasDe10Horas);
+		
+		
+		// Pto 4
+		
+		List<Vuelo> vuelosMasDe24Horas = vuelos.stream()
+				.filter(vuelo -> ChronoUnit.HOURS
+				    .between(LocalDateTime.of(vuelo.getFechaSalida(), vuelo.getHoraSalida()), 
+						  LocalDateTime.of(vuelo.getFechaLlegada(), vuelo.getHoraLlegada())) > 24)
+				.toList();
+				
+		System.out.println("Vuelos que demoran mas de un dia en llegar a su destino ");
+		System.out.println(vuelosMasDe24Horas);
+		
+		// Pto 5
+		
+		Map<Destino, List<Pasajero>> pasajerosPorDestino = vuelos.stream()
+				.collect(Collectors.toMap(Vuelo::getDestino, Vuelo::getPasajeros));
+		
+		// Pto 6
+		
+		List<Vuelo> vuelosUltimos10DiasDelMes = vuelos.stream()
+				.filter(vuelo -> vuelo.getFechaSalida()
+						.with(TemporalAdjusters.lastDayOfMonth())
+						     .minusDays(10).isBefore(vuelo.getFechaSalida()))
+				.toList();
+		
+		System.out.println("Vuelos que salen en los ultimos 10 dias del mes: ");
+		System.out.println(vuelosUltimos10DiasDelMes);
+		
+		// Ejercicio al margen: Respecto al Pto 6, ademas de lo que se pide, que la 
+		// fecha de salida se corresponda con el mes actual
 
+		List<Vuelo> vuelosUltimos10DiasDelMes2 = vuelos.stream()
+				.filter(vuelo -> vuelo.getFechaSalida()
+						.with(TemporalAdjusters.lastDayOfMonth())
+						     .minusDays(10).isBefore(vuelo.getFechaSalida()) && 
+						         vuelo.getFechaSalida().getMonth()
+						             .equals(LocalDate.now().getMonth()))
+				.toList();
+		
+		// Pto 7
+		
+		Map<Genero, Map<Long, List<Pasajero>>> pasajerosPorGeneroYEdad;
+		
+		pasajerosPorGeneroYEdad = vuelos.stream()
+				.flatMap(vuelo -> vuelo.getPasajeros().stream())
+				   .collect(Collectors.groupingBy(Pasajero::genero,
+						Collectors.groupingBy(pasajero -> ChronoUnit.YEARS
+								.between(pasajero.fechaNacimiento(), LocalDate.now()))));
+		
+		// Ejercicio al margen del Pto 7. En lugar de tener una lista de pasajeros, quiero
+		// una lista de nombres de pasajeros sin duplicados
+		
+		Map<Genero, Map<Long, Set<String>>> nombresDePasajerosPorGeneroYEdadSinDuplicados;
+		
+		nombresDePasajerosPorGeneroYEdadSinDuplicados = vuelos.stream()
+				.flatMap(vuelo -> vuelo.getPasajeros().stream())
+				   .collect(Collectors.groupingBy(Pasajero::genero,
+						Collectors.groupingBy(pasajero -> ChronoUnit.YEARS
+								.between(pasajero.fechaNacimiento(), LocalDate.now()), 
+						     Collectors.mapping(pasajero -> pasajero.nombre(), 
+						    		 Collectors.toSet()))));
+		
+		// Pto 8
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
